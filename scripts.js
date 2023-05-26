ebec5dbfa1f1e113c7025b6c04a37f72c1653cb8
Sam Newitt <samuel.newitt@gmail.com>
2023-05-25 21:30:17 -0500

    Added map

---
+++ scripts.js
@@ -0,0 +1,10 @@
+window.onload=init();
+
+function init(){
+    if($("svg-container").css("width")>$("svg-container").css("width"))
+    {
+    $("map").css("width",window.innerWidth);
+    $("map").css("height", window.innerHeight);
+    }
+
+}
