e11412d317eeda6dc9670eb3913a95254102b175
Sam Newitt <samuel.newitt@gmail.com>
2024-02-05 13:44:49 -0600

    Pinloader test

--- resources/pinLoader.js
+++ resources/pinLoader.js
@@ -25,7 +25,7 @@ pin.setAttribute("ypos","."+pinData.yPosition);
 pin.setAttribute("href","");
 pin.setAttribute("name",pinData.name);
 document.getElementsByClassName("svg-pan-zoom_viewport")[0].appendChild(pin);
-console.log(12);
+console.log(13);
 console.log(pin);
 
     });
--- resources/scripts.js
+++ resources/scripts.js
@@ -1,7 +1,6 @@
 var height, width, pinSize;
 var targetPin, mouseX, mouseY, mousePrevX, mousePrevY;
 var info, allInfos, allMedias, allCloses, allLogos;
-console.log(pins);
 
 var panZoom = svgPanZoom('#svg-container');
 
@@ -61,6 +60,8 @@ function resize(){
 }
 function init(){
 var pins = document.getElementsByClassName("pin");
+console.log(pins);
+
     setMap();
     resize();
 
