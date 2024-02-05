05d8575e41419a3807e236ddd25231791a7e01b3
Sam Newitt <samuel.newitt@gmail.com>
2024-02-05 13:00:27 -0600

    Pinloader test

--- resources/pinLoader.js
+++ resources/pinLoader.js
@@ -4,12 +4,11 @@ function pinLoader(){
     fetch('../MASTERLIST.json')
     .then((response) => response.json())
     .then((json) =>     {
-        console.log(4);
         fileList=json;
-        console.log(fileList);
-    
     });
 
+    console.log(5);
+    console.log(fileList);
 
 
     for(var i=0; i<fileList.length; i++){
