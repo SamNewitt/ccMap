7922e7842187ee600fc380bd29a3991aced45952
Sam Newitt <samuel.newitt@gmail.com>
2024-02-05 14:10:06 -0600

    Pinloader test

--- resources/pinLoader.js
+++ resources/pinLoader.js
@@ -24,8 +24,8 @@ pin.setAttribute("xpos","."+pinData.xPosition);
 pin.setAttribute("ypos","."+pinData.yPosition);
 pin.setAttribute("href","");
 pin.setAttribute("name",pinData.name);
-// document.getElementsByClassName("svg-pan-zoom_viewport")[0].appendChild(pin);
-console.log(15);
+document.getElementsByClassName("svg-pan-zoom_viewport")[0].appendChild(pin);
+console.log(16);
 console.log(pin);
 
     });
